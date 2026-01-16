ee5b9aff41e0e70a0606dde15b998395da740bf3
Luca Filip <filip.luca.work@gmail.com>
2026-01-16 11:17:20 +0200

    fix storybook issues

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,10 +1,49 @@
-import type { Preview } from '@storybook/angular'
+import {applicationConfig, Preview} from '@storybook/angular'
 import { setCompodocJson } from "@storybook/addon-docs/angular";
+import { moduleMetadata } from '@storybook/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import docJson from "../documentation.json";
+import {PrimeNG} from 'primeng/config';
+import Lara from '@primeng/themes/lara';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {provideHttpClient} from '@angular/common/http';
+import {APP_BOOTSTRAP_LISTENER} from '@angular/core';
 setCompodocJson(docJson);
 
+
+function provideTheme(config: PrimeNG): () => void {
+  return (): void => {
+    config.theme.set({
+      preset: Lara,
+      options: {
+        darkModeSelector: false,
+      },
+    });
+  };
+}
+
 const preview: Preview = {
+  decorators: [
+    moduleMetadata({
+      imports: [FormsModule, ReactiveFormsModule],
+    }),
+    applicationConfig({
+      providers: [
+        provideAnimations(),
+        provideHttpClient(),
+        {
+          provide: APP_BOOTSTRAP_LISTENER,
+          useFactory: provideTheme,
+          deps: [PrimeNG],
+          multi: true,
+        },
+      ],
+    }),
+  ],
   parameters: {
+    viewport: {
+      defaultViewport: 'responsive', // Set the default viewport
+    },
     controls: {
       matchers: {
        color: /(background|color)$/i,
